fcbc2ecad79e4ba190293b98b365ed367cb34035
Youssef M. Ezzat <118293173+iamyoussefezzat@users.noreply.github.com>
2024-11-15 23:04:59 +0200

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -537,8 +537,8 @@ const Home = () => {
                 <p>  باحثة عن المعلومات  </p>
               <div className='detil'>
 
-              <HashLink to="/#process"> <LinkedInIcon /> </HashLink>
-              <HashLink to="/#process"> <GitHubIcon /> </HashLink>
+              <HashLink to="https://www.linkedin.com/in/rana-gaber/"> <LinkedInIcon /> </HashLink>
+            
               <HashLink to="/#process"> <EmailIcon /> </HashLink>
              
               </div> 
@@ -559,7 +559,7 @@ const Home = () => {
 
             <div className='detil'>
             <HashLink to="http://www.linkedin.com/in/youssef-m-ezzat-63412b300"> <LinkedInIcon /> </HashLink>
-            <HashLink to="https://github.com/iamyoussefezzat/makeen"> <GitHubIcon /> </HashLink>
+            <HashLink to="https://github.com/iamyoussefezzat"> <GitHubIcon /> </HashLink>
             <HashLink to="youssefm.ezzat99@gmail.com"> <EmailIcon /> </HashLink>
              
               </div> 
